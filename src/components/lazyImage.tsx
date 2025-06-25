282452360e741b7bb2ff27c1597cc4adae02c001
Christian Montoya <chrimonlo@gmail.com>
2025-06-25 16:24:34 -0500

    feat(lazyImage): Add onLazyLoad prop to the LazyImageProps

--- src/components/lazyImage.tsx
+++ src/components/lazyImage.tsx
@@ -2,23 +2,35 @@ import type { ImgHTMLAttributes, JSX } from 'react';
 import { useRef, useEffect, useState } from 'react';
 type LazyImageProps = {
 	src: string;
+	onLazyLoad?: (img: HTMLImageElement) => void;
 };
 type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
 
 type Props = LazyImageProps & ImageNative;
-export const LazyImage = ({ src, alt, ...imgProps }: Props): JSX.Element => {
+export const LazyImage = ({ src, onLazyLoad, ...imgProps }: Props): JSX.Element => {
 	//Al usar useRef se coloca el tipo del elemento a referenciar y se inicializa en null en caso de no saber que valor por defecto deberia tener
 	const node = useRef<HTMLImageElement>(null);
+	//Se usa este usestate para evita r que se renderize mas de una vez cada imagen al realizar el lazy loading
+	const [isLazyLoaded, setIsLazyLoaded] = useState(false);
 	const [currentSrc, setCurrentSrc] = useState(
 		'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
 	);
 	useEffect(() => {
+		if (isLazyLoaded) {
+			return;
+		}
 		// Crear un nuevo observador, para hacer lazy loading de las imagenes
 		// Se va a cargar la imagen unicamente cuando sea visible en el viewport de la pantalla
 		const observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					setCurrentSrc(src);
+				if (!entry.isIntersecting || !node.current) {
+					return;
+				}
+				setCurrentSrc(src);
+				observer.disconnect();
+				setIsLazyLoaded(true);
+				if (typeof onLazyLoad === 'function') {
+					onLazyLoad(node.current);
 				}
 			});
 		});
@@ -30,7 +42,7 @@ export const LazyImage = ({ src, alt, ...imgProps }: Props): JSX.Element => {
 		return () => {
 			observer.disconnect();
 		};
-	}, [src]);
+	}, [src, onLazyLoad, isLazyLoaded]);
 
 	return (
 		<img
