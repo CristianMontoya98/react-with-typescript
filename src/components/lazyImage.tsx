3a2c08c9cea0b34616e8e632ed44bcf100c89a83
Christian Montoya <chrimonlo@gmail.com>
2025-06-25 10:42:19 -0500

    feat(lazyImage): Change the image prop by src

--- src/components/lazyImage.tsx
+++ src/components/lazyImage.tsx
@@ -1,15 +1,15 @@
 import type { ImgHTMLAttributes, JSX } from 'react';
 import { useRef, useEffect, useState } from 'react';
 type LazyImageProps = {
-	image: string;
+	src: string;
 };
 type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
 
 type Props = LazyImageProps & ImageNative;
-export const LazyImage = ({ image, alt, ...imgProps }: Props): JSX.Element => {
+export const LazyImage = ({ src, alt, ...imgProps }: Props): JSX.Element => {
 	//Al usar useRef se coloca el tipo del elemento a referenciar y se inicializa en null en caso de no saber que valor por defecto deberia tener
 	const node = useRef<HTMLImageElement>(null);
-	const [src, setSrc] = useState(
+	const [currentSrc, setCurrentSrc] = useState(
 		'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
 	);
 	useEffect(() => {
@@ -18,7 +18,7 @@ export const LazyImage = ({ image, alt, ...imgProps }: Props): JSX.Element => {
 		const observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
-					setSrc(image);
+					setCurrentSrc(src);
 				}
 			});
 		});
@@ -30,12 +30,12 @@ export const LazyImage = ({ image, alt, ...imgProps }: Props): JSX.Element => {
 		return () => {
 			observer.disconnect();
 		};
-	}, [image]);
+	}, [src]);
 
 	return (
 		<img
 			ref={node}
-			src={src}
+			src={currentSrc}
 			{...imgProps}
 		/>
 	);
